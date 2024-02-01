69b73737b411b7bd2a88274e0eea978cdb4368ec
nonrita <2220025@ecc.ac.jp>
2024-02-02 00:41:20 +0900

    init

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest"/>
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: true,
+  },
+
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./vitest-setup.ts'],
+  },
+});
